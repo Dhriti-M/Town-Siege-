fe6f38ac9791d3b41762275b3bfcdcbd089eab6a
Dhriti-M <65026480+Dhriti-M@users.noreply.github.com>
2020-06-28 22:31:02 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,35 @@
+class Box{
+  constructor(x, y, width, height, angle) {
+      var options = {
+          'restitution':1,
+          'friction':0.2,
+          'density':0.2
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("box.png");
+      World.add(world, this.body);
+      this.Visiblity=255;
+    }
+    display(){
+      if(this.body.velocity.x<3){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+      else{
+        World.remove(world, this.body);
+        push();
+        this.Visiblity = this.Visiblity - 5;
+        tint(255,this.Visiblity);
+        image(this.image, this.body.position.x, this.body.position.y,this.width,this.height);
+        pop();
+      }
+
+    }
+}
